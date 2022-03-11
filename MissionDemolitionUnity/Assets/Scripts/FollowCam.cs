bceca1d45c103c4559f0c3154cd6deaa059e5ebc
samuel-ricketts <78981392+samuel-ricketts@users.noreply.github.com>
2022-03-10 23:38:44 -0600

    Part2

---
+++ MissionDemolitionUnity/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,50 @@
+//Written by: Sammy Ricketts
+//Created: 2/20/22
+//Edited by: N/A
+//Last Edited: 2/20/22
+//Description: Slingshot code
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+    static public GameObject POI;
+    [Header("Set Dynamically")]
+    public float camZ; 
+
+
+void Awake() {
+camZ = this.transform.position.z;
+}
+
+
+void FixedUpdate () {
+if (POI == null) return; 
+Vector3 destination = POI.transform.position;
+destination.x = Mathf.Max( minXY.x, destination.x );
+destination.y = Mathf.Max( minXY.y, destination.y );
+destination = Vector3.Lerp(transform.position, destination, easing);
+destination.z = camZ;
+transform.position = destination;
+Camera.main.orthographicSize = destination.y + 10;
+
+}
+
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
